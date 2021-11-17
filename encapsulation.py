6e9b10e441d5feaba37b074f2aa36d4983ea582f
jahnavi gutti <82321859+jahnavi-gutti@users.noreply.github.com>
2021-11-17 23:28:57 +0530

    Create encapsulation.py

---
+++ encapsulation.py
@@ -0,0 +1,12 @@
+#What is a encapsulation in Python?
+#Encapsulation in Python is the process of wrapping up variables and methods into a single entity.
+class encap:
+    a=10
+    def display(self):
+        print("welcome")
+obj=encap()
+print(obj.a)
+obj.display()
+"""
+10
+welcome"""
